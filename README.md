f88c548e68808b008c036910f0dc89da5d0eaf80
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-06-12 16:06:15 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+#paciorki 
+
+the gola is to find the weakest link <br>
+-sanetra [ done ] 
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by studio25 on 12.06.2021.
+//
+#include <iostream>
+
+int main(){
+    std::cout<<"hellowowrdl;"
+    return 0;
+
+};
